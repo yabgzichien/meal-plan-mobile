cd35195dc3bef0e921b08ec20b080bc00b4c5b71
unknown <yangzicraft@gmail.com>
2022-04-30 17:16:07 +0800

    alphabets

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -157,6 +157,36 @@ const fetchCountries = async () =>{
         />
       </View>
 
+      <View>
+        <Text>A</Text>
+        <Text>B</Text>
+        <Text>C</Text>
+        <Text>D</Text>
+        <Text>E</Text>
+        <Text>F</Text>
+        <Text>G</Text>
+        <Text>H</Text>
+        <Text>I</Text>
+        <Text>J</Text>
+        <Text>K</Text>
+        <Text>L</Text>
+        <Text>M</Text>
+        <Text>N</Text>
+        <Text>O</Text>
+        <Text>P</Text>
+        <Text>Q</Text>
+        <Text>R</Text>
+        <Text>S</Text>
+        <Text>T</Text>
+        <Text>U</Text>
+        <Text>V</Text>
+        <Text>W</Text>
+        <Text>X</Text>
+        <Text>Y</Text>
+        <Text>Z</Text>
+    
+      </View>
+
 
       <Provider>
 
